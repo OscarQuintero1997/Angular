5e7b6790c445b38f8c2309fdbad89f901f0af5c0
OscarQuinter <devoscar1997@gmail.com>
2025-11-07 16:26:49 -0500

    feature: signal input and component creation

---
+++ 02-bases/src/app/components/character-add/character-add.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'app-character-add',
+  templateUrl: './character-add.component.html',
+})
+export class CharaAddComponent  {
+name = signal ('');
+power = signal (0);
+  characters = signal<any[]>([]);
+
+  AddCharacter() {
+  if (!this.name() || !this.power() || this.power() <= 0) {
+    return;
+  }
+
+  const newCharacter:Character = {
+    id: this.characters().length + 1,
+    name: this.name(),
+    power: this.power()
+  };
+  // this.characters.update(list => [...list, newCharacter]);
+
+  this.resetFils();
+}
+
+  resetFils() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
+}
