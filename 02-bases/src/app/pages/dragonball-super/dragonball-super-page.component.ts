ace994f33e2695d25132b1136194cac076a6fad1
OscarQuinter <devoscar1997@gmail.com>
2025-11-10 11:53:45 -0500

    feature: Services were created in Angular, Effects, and Local Storage, LinkedSignal - LocalStorage loading

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list';
-import type { Character } from '../../interfaces/character.interface';
 import { CharaAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { DragonballService } from '../../services/dragonball.service';
 
 @Component({
   selector: 'dragonball-super',
@@ -9,19 +9,11 @@ import { CharaAddComponent } from '../../components/dragonball/character-add/cha
   imports: [CharacterListComponent, CharaAddComponent],
 })
 export class dragonBallSuperPageComponent {
-  name = signal('');
-  power = signal(0);
+  // //Metodo tradicional de inyectar dependencias.
+  // constructor (
+  //   public DragonballService: DragonballService
+  // ) {}
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
-
-  AddCharacter(character: Character) {
-    this.characters.update((list)=> [...list, character]);
-  }
-  resetFils() {
-    this.name.set('');
-    this.power.set(0);
-  }
+  //Inyección de dependencias.
+  public dragonballService = inject(DragonballService);
 }
