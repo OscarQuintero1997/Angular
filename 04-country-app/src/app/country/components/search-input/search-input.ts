0698d8bcd08a030e2373c5685b9faf15d1ff28b6
OscarQuinter <devoscar1997@gmail.com>
2026-02-12 15:26:53 -0500

    <country-list></country-list>

---
+++ 04-country-app/src/app/country/components/search-input/search-input.ts
@@ -0,0 +1,12 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './search-input.html',
+})
+export class SearchInput {
+
+  placeholder = input<string>('Buscar');
+  value = output<string>();
+ }
